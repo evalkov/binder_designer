fc4c1b2ab789c1a9e7c2e7e10aa093b96c2b8f05
Eugene Valkov <118670165+evalkov@users.noreply.github.com>
2024-04-13 10:14:21 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,6 +54,19 @@ sed -i "s|schedule_directory_path: null|schedule_directory_path: ${INSTALL_DIR}/
 # Make a soft link to ProteinMPNN within the dl_binder_design directory
 ln -s ${INSTALL_DIR}/ProteinMPNN ${INSTALL_DIR}/dl_binder_design/mpnn_fr/ProteinMPNN
 
+# Download the model weights into the RFDiffusion directory
+mkdir -p ${INSTALL_DIR}/RFdiffusion/models
+cd ${INSTALL_DIR}/RFdiffusion/models
+wget http://files.ipd.uw.edu/pub/RFdiffusion/6f5902ac237024bdd0c176cb93063dc4/Base_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/e29311f6f1bf1af907f9ef9f44b8328b/Complex_base_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/60f09a193fb5e5ccdc4980417708dbab/Complex_Fold_base_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/74f51cfb8b440f50d70878e05361d8f0/InpaintSeq_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/76d00716416567174cdb7ca96e208296/InpaintSeq_Fold_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/5532d2e1f3a4738decd58b19d633b3c3/ActiveSite_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/12fc204edeae5b57713c5ad7dcb97d39/Base_epoch8_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/f572d396fae9206628714fb2ce00f72e/Complex_beta_ckpt.pt
+wget http://files.ipd.uw.edu/pub/RFdiffusion/1befcb9b28e2f778f53d47f18b7597fa/RF_structure_prediction_weights.pt
+
 # Download and extract AlphaFold2 model weights
 mkdir -p ${INSTALL_DIR}/dl_binder_design/af2_initial_guess/model_weights/params
 cd ${INSTALL_DIR}/dl_binder_design/af2_initial_guess/model_weights/params
